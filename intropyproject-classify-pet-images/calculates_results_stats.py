9bd66438b18db5935c5bf6c485b91dda777d7fc2
Daniel Tejeda <dtejeda@gmail.com>
2019-01-09 19:52:57 -0800

    Final

--- intropyproject-classify-pet-images/calculates_results_stats.py
+++ intropyproject-classify-pet-images/calculates_results_stats.py
@@ -79,7 +79,6 @@ def calculates_results_stats(results_dic):
     results_stats_dic['n_correct_dogs'] = 0
     results_stats_dic['n_correct_notdogs'] = 0
     results_stats_dic['n_correct_breed'] = 0
-    results_stats_dic['n_label_matches'] = 0
 
     #Counts
 
